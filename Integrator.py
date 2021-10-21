0e0bda8acef177ac66dc273582133f9ba9b94382
FMagnani <federico.magnani5@studio.unibo.it>
2021-10-21 10:31:42 +0200

    Added Euler scheme as a method od Integrator

--- Integrator.py
+++ Integrator.py
@@ -15,19 +15,21 @@ import matplotlib.pyplot as plt
 # Made in general reference frame -> d/dt (Mean phase) is not 0
 class Integrator():
     
-    def __init__(self, N_oscillators, init_distribution, frequencies, coupling):
+    def __init__(self, init_distribution, frequencies, coupling, temperature):
         
-        # TODO
-        # 1 Maybe put warning if N very large
-        # 2 Phi stuff: it can be a vector or a distribution from which withdraw
-        # 3 Scheme stuff: it can be a function or a name
-        # 4 Noise?
+
+        if not (len(init_distribution) == len(frequencies)):
+            raise ValueError("init_distribution and frequencies should have same length")
         
-        self.N = N_oscillators
         self.phi = np.array(init_distribution)
         self.w = np.array(frequencies)
+        self.T = temperature
         self.k = coupling
         self.coherence, self.psi = self.get_orderPars()
+        
+        self.scheme_dict = {
+                            'euler': self.Euler
+                            }
         
     def get_orderPars(self):
         """
@@ -38,8 +40,6 @@ class Integrator():
             Amplitude (length) of the center-of-mass vector
             r = SQRT[ (1/N Sum_j cos(phi_j))^2 + (1/N Sum_j sin(phi_j))^2 ]
         """
-        
-        
         psi = np.mean(self.phi)
         
         cosines = np.cos(self.phi)
@@ -50,8 +50,36 @@ class Integrator():
 
         return coherence, psi
 
-    def integrate(self, Dt, iterations, numerical_scheme):
+
+    def integrate(self, Dt, iterations, numerical_scheme, seed):
+        """
         
+
+        Parameters
+        ----------
+        Dt : float
+            Time interval for integration.
+        iterations : int
+            Total number of iterations.
+        numerical_scheme : str
+            One of: 
+                'euler'
+                'heun'
+                'taylor' (Taylor 3/2 is used)
+        seed : int
+            Seed for generation of random numbers for the Wiener process.
+
+        Returns
+        -------
+        phi_history : np.array with shape (len(phi), iterations)
+            Phase of each oscillator at each iteration.
+        coherence_history : np.array with shape (len(phi))
+            The coherence parameter at each iteration.
+        psi_history : np.array with shape (len(phi))
+            The mean phase at each iteration.
+        """
+        
+        scheme = self.scheme_dict[numerical_scheme]
         
         coherence_history = np.array([self.coherence])
         psi_history = np.array([self.psi])
@@ -61,7 +89,7 @@ class Integrator():
          
         for i in range(iterations):
             
-            self.phi = numerical_scheme(self.phi, self.w, self.k, Dt)
+            self.phi = scheme(Dt, seed)
             
             # Pay attention to the following AWFUL syntax. It's not obvious.
             phi_history = np.append(phi_history, [self.phi], axis=0)
@@ -72,38 +100,42 @@ class Integrator():
             
         return phi_history, coherence_history, psi_history
 
+    
+    # NUMERICAL SCHEMES
+    
+    def Euler(self, Dt, seed):
+    
+        # Compute all combinations between phi's entries
+        xx, yy = np.meshgrid(self.phi,self.phi)        
+    
+        # Apply all pairwise differences in parallel
+        distances = xx - yy
+    
+        # Apply sine to all the differences in parallel
+        sines = np.sin(distances)
+    
+        # Forcing term
+        forcings = self.k*np.mean(sines, axis=1)
+    
+        # Noise
+        np.random.seed(seed)
+        noise = self.T*np.random.normal(0,np.sqrt(Dt),len(self.phi))
+    
+        new_phi = self.phi + Dt*(freqs + forcings + noise)
+    
+        # New phi modulo 2pi
+        new_phi = np.mod(new_phi, 2*np.pi)
+    
+        return new_phi
 
-#%%
 
-
-
-def EulerKuramoto(phi, freqs, coupling, Dt):
-
-    # Compute all combinations between phi's entries
-    xx, yy = np.meshgrid(phi,phi)        
-    
-    # Apply all pairwise differences in parallel
-    distances = xx - yy
-    
-    # Apply sine to all the differences in parallel
-    sines = np.sin(distances)
-    
-    # Forcing term
-    forcings = coupling*np.mean(sines, axis=1)
-    
-    new_phi = phi + Dt*(freqs + forcings)
-    
-    # New phi modulo 2pi
-    new_phi = np.mod(new_phi, 2*np.pi)
-    
-    return new_phi
 
 
 #%%
 
 # Script example
 
-N = 100
+N = 2
 
 np.random.seed(123)
 
@@ -115,15 +147,14 @@ init_phi = np.multiply(init_phi_distr, 2*np.pi)
 
 freqs = np.random.normal(0,1,N)
 
-K = 2.2
-system = Integrator(N, init_phi, freqs, K)
-phi, r, psi = system.integrate(0.01, 1000, EulerKuramoto)
-# plt.plot(phi)
-# plt.plot(r, 'k')
-# TODO
-# FIX: Why r goes above 1?
-# I didn't set the seed. Now r is ok. I will never fix that :(
+for i in range(21)[18:]:
 
+    K = 2.2
+    T = i
+    system = Integrator(init_phi, freqs, K, T)
+    phi, r, psi = system.integrate(0.01, 1000, 'euler', 123)
+    plt.plot(phi)
+    # plt.plot(r, 'k')
 
 
 # system = Integrator(2, [0,1], [1,2], 3)
